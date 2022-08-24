1ccfe5e656f9dca202822096c27d65e88555d4e6
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-08-24 14:53:58 +0100

    shopping cart functionality added

--- scripts.js
+++ scripts.js
@@ -31,12 +31,29 @@ const filterBooks = () => {
   setCards(newBooks);
 };
 
+let shoppingCart = [];
+
 const setCards = (books) => {
   const MAIN_ROW = document.querySelectorAll("#mainRow");
   for (let i = 0; i < books.length; i++) {
     MAIN_ROW[0].innerHTML += `
         <div class="col-md-4">
+        
         <div class="card mt-3" style="width: 18rem">
+        <span class="badge badge-success position-absolute display-6 d-none">
+        <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-basket"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"
+              />
+            </svg>
+        </span>
           <img class="card-img-top" src="..." alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
@@ -87,7 +104,43 @@ const setCards = (books) => {
   CARD_CATEGORY.forEach((elem, i) => {
     elem.innerHTML = "Category: " + books[i].category;
   });
+
+  const ADD_BUTTONS = document.querySelectorAll(
+    ".btn-group > .btn:first-child"
+  );
+  const ADDED_BADGES = document.querySelectorAll(".badge");
+  
+  ADD_BUTTONS.forEach((elem, i) => {
+    elem.addEventListener("click", () => {
+      ADDED_BADGES[i].classList.toggle("d-none");
+      shoppingCart.push(books[i]);
+      const LIST_CONT = document.getElementById("shopListCont");
+      LIST_CONT.innerHTML = ""
+      for (let v = 0; v < shoppingCart.length; v++) {
+        LIST_CONT.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+                  <div class="ms-2 me-auto">
+                    <div class="fw-bold">${books[v].title}</div>
+                    ${books[v].category}
+                  </div>
+                  <span class="badge bg-primary rounded-pill">£${books[v].price.toString()}</span>
+        </li>
+        `
+      }
+    });
+  });
 };
+
+const EMPTY_BUTTON = document.getElementById("emptyCart");
+EMPTY_BUTTON.addEventListener("click", () => {
+    shoppingCart = [];
+    const LIST_CONT = document.getElementById("shopListCont");
+    LIST_CONT.innerHTML = "";
+    const ADDED_BADGES = document.querySelectorAll(".badge");
+    ADDED_BADGES.forEach((elem) => elem.classList.add("d-none"));
+})
+
+const addToCart = () => {};
 
 loadBooks();
 
